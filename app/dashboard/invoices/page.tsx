8b11ae40b1d40f1476b64895518dbae0a35f59b0
kawaguchi <21010402@anabuki-college.ac.jp>
2023-12-18 17:14:23 +0900

    16

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,16 +6,23 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+}
+
 export default async function Page({
   searchParams,
 }: {
   searchParams?: {
     query?: string;
     page?: string;
-  };
+  }
 }) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
+
   const totalPages = await fetchInvoicesPages(query);
 
   return (
@@ -29,9 +36,9 @@ export default async function Page({
       </div>
        <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
-      </Suspense> 
+      </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        <Pagination totalPages={totalPages} /> 
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
--- app/ui/invoices/[id]/edit/page.tsx
+++ app/ui/invoices/[id]/edit/page.tsx
@@ -1,18 +1,25 @@
 import Form from '@/app/ui/invoices/edit-form';
-import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
 
-import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data'; 
+export const metadata: Metadata = {
+  title: 'Invoice Update',
+}
+
 export default async function Page({ params }: { params: { id: string } }) {
-    const id = params.id;
-    const [invoice, customers] = await Promise.all([
-        fetchInvoiceById(id),
-        fetchCustomers(),
-      ]);
-      if (!invoice) {
-        notFound();
-      }
-    return (
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  if (!invoice) {
+    notFound();
+  }
+
+  return (
     <main>
       <Breadcrumbs
         breadcrumbs={[
